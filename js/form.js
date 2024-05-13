3b272c6e3bccd8c1ff7d4764ab6cc7c253c69e54
Gabrielc30 <149747571+Gabrielc30@users.noreply.github.com>
2024-05-12 22:39:30 -0300

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -6,6 +6,11 @@ const expresiones = {
 	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 }
 
+const campos = {
+    nombre: false,
+    email: false
+}
+
 const validarFormulario = (e) => {
 	switch (e.target.name) {
 		case "nombre":
@@ -15,7 +20,7 @@ const validarFormulario = (e) => {
 				document.querySelector('#grupo__nombre i').classList.add('bi-check-circle-fill');
 				document.querySelector('#grupo__nombre i').classList.remove('bi-x-circle-fill');
 				document.querySelector('#grupo__nombre .formulario__input-error').classList.remove('formulario__input-error-activo');
-
+                campos['nombre'] = true;
 
 			} else {
 				document.getElementById('grupo__nombre').classList.add('formulario__grupo-incorrecto');
@@ -23,7 +28,8 @@ const validarFormulario = (e) => {
 				document.querySelector('#grupo__nombre i').classList.add('bi-x-circle-fill');
 				document.querySelector('#grupo__nombre i').classList.remove('bi-check-circle-fill');
 				document.querySelector('#grupo__nombre .formulario__input-error').classList.add('formulario__input-error-activo');
-			}
+                campos['nombre'] = false;
+            }
 			
 			if(e.target.value <= 0){
 				document.querySelector('#grupo__nombre i').classList.remove('bi-x-circle-fill');
@@ -37,7 +43,7 @@ const validarFormulario = (e) => {
 				document.querySelector('#grupo__email i').classList.add('bi-check-circle-fill');
 				document.querySelector('#grupo__email i').classList.remove('bi-x-circle-fill');
 				document.querySelector('#grupo__email .formulario__input-error').classList.remove('formulario__input-error-activo');
-
+                campos['email'] = true;
 
 			} else {
 				document.getElementById('grupo__email').classList.add('formulario__grupo-incorrecto');
@@ -45,7 +51,8 @@ const validarFormulario = (e) => {
 				document.querySelector('#grupo__email i').classList.add('bi-x-circle-fill');
 				document.querySelector('#grupo__email i').classList.remove('bi-check-circle-fill');
 				document.querySelector('#grupo__email .formulario__input-error').classList.add('formulario__input-error-activo');
-			}
+                campos['email'] = false;
+            }
 
 			if(e.target.value <= 0){
 				document.querySelector('#grupo__email i').classList.remove('bi-x-circle-fill');
@@ -63,5 +70,21 @@ inputs.forEach((input)=> {
 
 formulario.addEventListener('submit', (e) => {
 	e.preventDefault();
+    const terminos = document.getElementById('terminos');
+
+    if(campos.nombre && campos.email && terminos.checked) {
+        formulario.reset();
+        document.querySelector('#grupo__nombre i').classList.remove('bi-check-circle-fill');
+        document.querySelector('#grupo__email i').classList.remove('bi-check-circle-fill');
+        document.querySelector('#grupo__enviar span').classList.add('formulario__mensaje-exito-activo');
+    } else {
+        document.querySelector('#formulario__mensaje').classList.add('formulario__mensaje-activo');
+    }
+
 
 })
+
+function removeMensaje() {
+    document.querySelector('#grupo__enviar span').classList.remove('formulario__mensaje-exito-activo');
+    document.querySelector('#formulario__mensaje').classList.remove('formulario__mensaje-activo')
+}
